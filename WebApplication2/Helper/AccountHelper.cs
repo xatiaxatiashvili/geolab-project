499febd1cfb390dc74fc9485a9f5b3a6dd984fe4
khatiakhatiashvili <khatia.khatiashvili12@gmail.com>
2020-02-07 14:34:27 +0400

    geolab

---
+++ WebApplication2/Helper/AccountHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace WebApplication2.Helper
+{
+    public class AccountHelper
+    {
+       
+            public static readonly string authSecret = "e20f2b0a644e68aa243b153937b28668f4270639d3de8f5de1aabae2927df82a";
+        public static string GetHash256(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string RandomString()
+            {
+                Random generator = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 10)
+              .Select(s => s[generator.Next(s.Length)]).ToArray());
+            
+            }
+
+    }
+}
